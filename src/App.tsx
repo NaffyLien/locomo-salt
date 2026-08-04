ddc3d1a7d58defa1af38440f401eea94d7cd509b
ANDRIANAIVO Nafafisoa <nafafyandrianaivo@gmail.com>
2026-08-04 09:10:43 +0300

    The React Mental Model
        Components: You built a UI out of a single function.
    
        State: You used useState to make your app "remember" the list of tasks.
    
        One-Way Data Flow: Data flows down into the HTML. Events flow up via functions like onClick.
    
        Immutability: You never changed the tasks array directly (e.g., tasks.push()). You always created a new array ([...tasks] or .filter()) and passed it to setTasks. This is crucial in React.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,50 @@
 import './App.css'
-import {useState} from 'react'
+import { useState } from 'react'
 
 function App() {
+  const maxTasks = 9
+  
   const [tasks, setTasks] = useState(["Learn React", "Build Project", "Go for a walk"])
-  return <div>
-      <h1>My Task Master</h1>
-      {
-        <ul>
-          {tasks.map((task, index)=>(
-            <li key={index}>{task}</li>
-          ))}
-        </ul>
-      }
+  const [newTask, setNewTask] = useState('')
 
-      <button onClick={()=>setTasks(["Hug the cat", ...tasks])}> Add</button>
-    </div>
+  const taskLeft = maxTasks - tasks.length
+
+  const handleAddNewTask = () => {
+    if (newTask.trim() !== "" && tasks.length < maxTasks) {
+      setTasks([...tasks, newTask])
+      setNewTask("")
+    }
+  }
+
+  function handleDeleteTask(index: number) {
+    setTasks(tasks.filter((_, idx) => (
+      idx !== index
+    )))
+  }
+
+  return <div>
+    <h1>My Task Master</h1>
+    <section>
+      <input
+        type='text'
+        placeholder='Insert next task here'
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+      />
+      <button onClick={handleAddNewTask}>Add task</button>
+    </section>
+
+    <ul>
+      {tasks.map((task, index) => (
+        <div key={index}>
+          <li key={index}>{task}</li>
+          <button onClick={() => handleDeleteTask(index)}>Delete</button>
+        </div>
+      ))}
+    </ul>
+
+    <p>You have {taskLeft} tasks left</p>
+  </div>
 }
 
 export default App
