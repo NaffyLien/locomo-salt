d00c90e7ecbb79f8e00bfb84cdb9fc49c508d71c
ANDRIANAIVO Nafafisoa <nafafyandrianaivo@gmail.com>
2026-08-10 22:40:55 +0300

    feat: enhance task management UI and improve styling

--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -1,50 +1,25 @@
-import type { Task } from '../../type'
-import TaskEdit from '../TaskEdit'
+import OneTask from '../OneTask'
 import './taskList.css'
 import { useState } from 'react'
-
-type TaskListProps = {
-  tasks: Task[],
-  deleteTask: (index: number) => void
-  finishTask: (index: number) => void
-  editTask: (index: number, task: Task) => void
-}
+import type { TaskListProps } from '../../type'
 
 const TaskList = ({ tasks, deleteTask, finishTask, editTask }: TaskListProps) => {
   const [edit, setEdit] = useState<number | null>()
 
   return <section className='taskList'>
     <h2>Task lists</h2>
-    <ul>
+    <div className='list'>
       {tasks.map((task, index) => (
-        <div key={index}>
-          <li key={index}>
-            <input
-              type="checkbox"
-              checked={task.completed}
-              id={`task-${index}`}
-              onChange={() => finishTask(index)}
-            />
-            <label
-              htmlFor={`task-${index}`}
-              className={task.completed ? 'taskFinish true' : 'taskFinish false'}
-              onDoubleClick={() => setEdit(index)}
-            >
-              {task.name + " "}
-              {task.dueDate ? task.dueDate : null}
-              {task.category}
-            </label>
-            <button onClick={() => deleteTask(index)}>Delete</button>
-          </li>
-          {edit == index && <TaskEdit
-            idx={index}
-            modifTask={task}
-            editTask={editTask}
-            clearTasks={() => setEdit(null)}
-          />}
-        </div>
+        <OneTask
+          key={index}
+          idx={index}
+          task={task}
+          deleteTask={deleteTask}
+          finishTask={finishTask}
+          editTask={editTask}
+        />
       ))}
-    </ul>
+    </div>
 
   </section>
 }
