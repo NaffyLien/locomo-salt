b0ff54956bf598b7a2e542720679365436734bd8
ANDRIANAIVO Nafafisoa <nafafyandrianaivo@gmail.com>
2026-08-11 14:11:48 +0300

    fix: build error class undefined

--- src/type.ts
+++ src/type.ts
@@ -1,22 +1,22 @@
 export type Task = {
   name: string,
   dueDate?: string,
-  category: "Work" | "Study" | "Personal" | "Shopping" | "Other"
-  details?: string
+  category: "Work" | "Study" | "Personal" | "Shopping" | "Other" | undefined,
+  details?: string,
   completed: boolean,
 }
 
 export type TaskListProps = {
   tasks: Task[],
-  deleteTask: (index: number) => void
-  finishTask: (index: number) => void
-  editTask: (index: number, task: Task) => void
+  deleteTask: (index: number) => void,
+  finishTask: (index: number) => void,
+  editTask: (index: number, task: Task) => void,
 }
 
 export type TaskProps = {
   idx: number,
   task: Task,
-  deleteTask: (index: number) => void
-  finishTask: (index: number) => void
-  editTask: (index: number, task: Task) => void
+  deleteTask: (index: number) => void,
+  finishTask: (index: number) => void,
+  editTask: (index: number, task: Task) => void,
 }
